c9035e9700113d328b7660e0f875ff0bb9b1e68d
Filipe Oliveira <contato@fmoliveira.com.br>
2020-03-12 03:27:37 -0300

    Setup apollo provider.

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import Home from './src/components/Home';
+import graphqlClient from './src/sdk/graphqlClient';
 
 export default function App() {
   return (
-    <Home />
+    <ApolloProvider client={graphqlClient}>
+      <Home />
+    </ApolloProvider>
   );
 }
