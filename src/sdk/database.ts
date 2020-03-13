13a06d305f8ca739637e1165cdc241114aba077d
Filipe Oliveira <contato@fmoliveira.com.br>
2020-03-13 04:12:40 -0300

    Create RxDB collection for todos.

--- src/sdk/database.ts
+++ src/sdk/database.ts
@@ -2,6 +2,8 @@ import RxDB from "rxdb";
 import SQLite from "react-native-sqlite-2";
 import SQLiteAdapterFactory from "pouchdb-adapter-react-native-sqlite";
 
+import todoSchema from "./todo-schema.json";
+
 const SQLiteAdapter = SQLiteAdapterFactory(SQLite);
 
 RxDB.plugin(SQLiteAdapter);
@@ -11,6 +13,10 @@ async function createDatabase() {
   const database = await RxDB.create({
     name: "tododb",
     adapter: "react-native-sqlite"
+  });
+  await database.collection({
+    name: "todo",
+    schema: todoSchema
   });
   return database;
 }
