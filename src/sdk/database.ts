0b1efebcfe55642fe7a412e91d936a9f90780e05
Filipe Oliveira <contato@fmoliveira.com.br>
2020-03-13 04:05:11 -0300

    Setup PouchDB adapter for RxDB.

--- src/sdk/database.ts
+++ src/sdk/database.ts
@@ -1,9 +1,16 @@
 import RxDB from "rxdb";
+import SQLite from "react-native-sqlite-2";
+import SQLiteAdapterFactory from "pouchdb-adapter-react-native-sqlite";
+
+const SQLiteAdapter = SQLiteAdapterFactory(SQLite);
+
+RxDB.plugin(SQLiteAdapter);
+RxDB.plugin(require("pouchdb-adapter-http"));
 
 async function createDatabase() {
   const database = await RxDB.create({
     name: "tododb",
-    adapter: "idb"
+    adapter: "react-native-sqlite"
   });
   return database;
 }
